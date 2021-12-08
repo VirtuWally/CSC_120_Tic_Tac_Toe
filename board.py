7782745238fd34f3eba81b7cccdbab4c7fc5c79b
Leon Henry Squire <lhsquire@waketech.edu>
2021-12-08 01:35:00 -0500

    Player 1 and 2 can alternate turns functionality added

--- board.py
+++ board.py
@@ -1,11 +1,17 @@
 # Title:  CSC.120 Software Engineering Project
 # SubTitle:  Tic Tac Toe
 # Author:  Leon Henry Squire
-# Version Stage 1
+# Version Stage 2
+# Players 1 and 2 can alternate turns
 
-# Tic-Tac-Toe grid is a 3 x 3 matrix implemented with a lists of lists
-# '-' represents an empty space on the board
-ttt_grid = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-print('Printing board')
-for i in range(0,3):
-    print(ttt_grid[i])
+def main
+    # Tic-Tac-Toe grid is a 3 x 3 matrix implemented with a lists of lists
+    # '-' represents an empty space on the board
+    ttt_grid = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+
+def print_board(ttt_g)
+    print('Printing board')
+    for i in range(0,3):
+      print(ttt_g[i])
+
+main()
