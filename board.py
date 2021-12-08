62dd1a9049c064d9760cfac4fc65cfec58d8031d
Leon Henry Squire <lhsquire@waketech.edu>
2021-12-08 01:00:48 -0500

    Board Printing funcionality added

---
+++ board.py
@@ -0,0 +1,11 @@
+# Title:  CSC.120 Software Engineering Project
+# SubTitle:  Tic Tac Toe
+# Author:  Leon Henry Squire
+# Version Stage 1
+
+# Tic-Tac-Toe grid is a 3 x 3 matrix implemented with a lists of lists
+# '-' represents an empty space on the board
+ttt_grid = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+print('Printing board')
+for i in range(0,3):
+    print(ttt_grid[i])
