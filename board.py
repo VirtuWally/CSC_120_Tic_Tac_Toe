fd676979bc0e709303cb98e95572afe781547753
Leon Henry Squire <lhsquire@waketech.edu>
2021-12-09 18:24:58 -0500

    Stage 2: alternate players, invalid location error, occupied space error, features from Stage 1

--- board.py
+++ board.py
@@ -12,6 +12,7 @@ def main():
     players = {}
     ttt_row = 0
     ttt_column = 0
+    move_count = 0  # keep track of total moves; when equals 3, begin checking for a winner
 
     # Player definitions
     player1 = {'Player1': 'X'}
@@ -27,14 +28,40 @@ def main():
         for i in range(1, 3):
             if i == 1:
                 # get player1 input
-                row, column = get_move(player1)
-                if empty_slot(ttt_row,ttt_column,ttt_grid):
-                    ttt_grid[ttt_row][ttt_column] = player1['Player1']
+                while True:
+                    ttt_row, ttt_column = get_move(player1)
+                    if empty_slot(ttt_row, ttt_column, ttt_grid):
+                        ttt_grid[ttt_row][ttt_column] = player1['Player1']
+                        out_str = 'Player 1 added mark at location {},{}'
+                        print(out_str.format(ttt_row, ttt_column))
+                        print_board(ttt_grid)
+                        move_count += 1
+                        break
+                    else:
+                        # slot is not empty
+                        out_str = '**** Board[{}][{}] has already been selected.  Please place marker somewhere ' \
+                                  'else on the board'
+                        print(out_str.format(ttt_row,ttt_column))
+                        print('**** Invalid choice.  Please mark again! ****')
+                        print_board(ttt_grid)
             else:
                 # get player2 input
-                row, column = get_move(player2)
-                if empty_slot(ttt_row,ttt_column,ttt_grid):
-                    ttt_grid[ttt_row][ttt_column] = player2['Player2']
+                while True:
+                    ttt_row, ttt_column = get_move(player2)
+                    if empty_slot(ttt_row, ttt_column, ttt_grid):
+                        ttt_grid[ttt_row][ttt_column] = player2['Player2']
+                        out_str = 'Player 2 added mark at location {},{}'
+                        print(out_str.format(ttt_row, ttt_column))
+                        print_board(ttt_grid)
+                        move_count += 1
+                        break
+                    else:
+                        # slot is not empty
+                        out_str = '**** Board[{}][{}] has already been selected.  Please place marker somewhere ' \
+                                  'else on the board'
+                        print(out_str.format(ttt_row,ttt_column))
+                        print('**** Invalid choice.  Please mark again! ****')
+                        print_board(ttt_grid)
 
 
 def empty_slot(row, column, ttt_grid):
@@ -48,20 +75,24 @@ def empty_slot(row, column, ttt_grid):
 def get_move(player):
     # player's move is added to the grid
     # determine if this is this player 1 or player 2
-    player_name = player.keys()
+    player_name = list(player.keys())
     if player_name[0] == 'Player1':
         player_num = 1
     else:
         player_num = 2
+    while True:
+        out_str = 'Player {}, make your move'
+        print(out_str.format(player_num))
 
-    out_str = 'Player {}, make your move'
-    print(out_str.format(player_num))
-
-    # get row and column number of the player's move
-    row = int(input('Enter row nos (0-2):'))
-    col = int(input('Enter col nos (0-2):'))
-
-    return row, col
+        # get row and column number of the player's move
+        row = int(input('Enter row nos (0-2):'))
+        col = int(input('Enter col nos (0-2):'))
+        if not (0 <= row <=2) or not (0 <= col <=2):
+            print('**** Invalid row or column.  Please select row / col between values 0 to 2 ****')
+            print('**** Invalid choice.  Please mark again! ****')
+            continue
+        else:
+            return row, col
 
 
 def print_board(ttt_g):
